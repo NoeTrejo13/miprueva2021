9522bfa0dbed1eafc4e221a7c4b88b3f2776232a
Noé Trejo <trejo.noe9513@gmail.com>
2021-06-24 19:03:35 -0600

    tercer commit

---
+++ miprueva2021/wwwroot/js/val.js
@@ -0,0 +1,9 @@
+﻿function validar(e) {
+    if (document.getElementById("flexCheckDefault").checked) {
+        return true;
+    }
+    else {
+        alert("El formulario no puede ser envia si no acepta los terminos");
+        return false;
+    }
+}
