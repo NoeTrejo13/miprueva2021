033ec7e638e2529c2f066ba4502c049033b6653a
Noé Trejo <trejo.noe9513@gmail.com>
2021-06-24 23:41:27 -0600

    cuarto commit

--- miprueva2021/Data/Migrations/20210625044441_migrax.Designer.cs
+++ miprueva2021/Data/Migrations/20210625044441_migrax.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using miprueva2021.Data;
 
 namespace miprueva2021.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210625044441_migrax")]
+    partial class migrax
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ miprueva2021/wwwroot/js/map.js
@@ -0,0 +1,24 @@
+﻿function findMe() {
+	var output = document.getElementById('map');
+
+	// Verificar si soporta geolocalizacion
+	if (navigator.geolocation) {
+		let map;
+
+		var latitude = posicion.coords.latitude;
+		var longitude = posicion.coords.longitude;
+
+		map = new google.maps.Map(document.getElementById("map"), {
+			center: { lat: latitude, lng: longitude },
+			zoom: 8,
+		});
+		
+	} else {
+		output.innerHTML = "<p>Tu navegador no soporta Geolocalizacion</p>";
+	}
+
+	function error() {
+		output.innerHTML = "<p>No se pudo obtener tu ubicación</p>";
+
+	}
+}
